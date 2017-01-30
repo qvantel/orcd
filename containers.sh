c83a7445faf02b7d3a7d6f39a456363b94f907ed
Johan Bjäreholt <johan@bjareho.lt>
2017-01-30 16:23:13 +0100

    Added frontend container

--- containers.sh
+++ containers.sh
@@ -12,27 +12,45 @@ GRAPHITE=nickstenning/graphite:$GRAPHITE_VERSION
 BACKEND_VERSION="latest"
 BACKEND=backend:$BACKEND_VERSION
 
+# Grafana
+GRAFANA_VERSION="latest"
+GRAFANA=grafana:$GRAFANA_VERSION
+
 usage="Usage: [start|stop|help]"
 
 case "$1"
 in
 	"start")
-		echo "NOTICE: All docker containers are not yet implemented"
+		# Cassandra container
 		docker run --name cassandra -d $CASSANDRA
+
+		# Graphite container
 		docker run --name graphite -d $GRAPHITE
 
+		# Backend container
 		(cd ./QvantelBackend; sbt assembly)
 		if [[ "$(docker images -q $BACKEND 2> /dev/null)" == "" ]]; then
 			docker rmi $BACKEND
 		fi
 		docker build -t backend ./QvantelBackend
 		docker run --name backend -p 8080:8080 -d $BACKEND
+
+		# Frontend container
+		if [[ $OSTYPE == *"darwin"* ]]; then
+			GRAFANA_VOLUME_TARGET=$HOME/grafana
+		else
+			GRAFANA_VOLUME_TARGET=/var/lib/grafana
+		fi
+		docker run -d --name frontend \
+			-p 3000:3000 \
+			-v $GRAFANA_VOLUME_TARGET:/var/lib/grafana \
+			grafana/grafana
 	;;
 	"stop")
 		echo "Stopping containers"
-		docker stop cassandra graphite backend > /dev/null
+		docker stop cassandra graphite backend frontend > /dev/null
 		echo "Removing containers"
-		docker rm cassandra graphite backend > /dev/null
+		docker rm cassandra graphite backend frontend > /dev/null
 	;;
 	"help"|*)
 		echo -e $usage
