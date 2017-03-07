2059c431dba60fbbb7f8a2289ae3ead8ee66081e
Erik Lilja <erikvlilja@gmail.com>
2017-03-07 14:38:28 +0100

    Split container in smaller pieces

--- containers.sh
+++ containers.sh
@@ -38,50 +38,48 @@ usage="Usage: [start|stop|clean|help]"
 GRAFANA_VOLUME_TARGET=$HOME/grafana
 CASSANDRA_VOLUME_TARGET=$HOME/cassandra
 
+function cassandra {
+    # Cassandra container
+    # Port: 9042
+    if [ ! "$(docker ps --all | grep cassandra)" ]; then
+        echo -e $YELLOW"### Creating cassandra container"$RESET
+        docker build -t cassandra ./Cassandra
+        docker run \
+            --restart=always \
+            --name cassandra \
+            -p $CASSANDRA_PORT:$CASSANDRA_PORT \
+            -d cassandra
 
-case "$1"
-in
-    "start")
-        # Cassandra container
-        # Port: 9042
-        if [ ! "$(docker ps --all | grep cassandra)" ]; then
-            echo -e $YELLOW"### Creating cassandra container"$RESET
-	    docker build -t cassandra ./Cassandra
-            docker run \
-                --restart=always \
-                --name cassandra \
-                -p $CASSANDRA_PORT:$CASSANDRA_PORT \
-                -d cassandra
-
-            if [ -n "$(docker ps | grep cassandra)" ]
-            then
-              echo "Waiting for port to open"
-              while [ -n "$(docker exec -it cassandra cqlsh -e exit 2>&1 | grep '\(e\|E\)rror')" ]
-              do
-                sleep 1
-              done
-              echo "Cqlsh is up and running"
-              echo "Running schema"
-              docker exec -it cassandra cqlsh -f /schema.cql
-            fi
-
-        else
-            echo -e $GREEN"### Restarting cassandra container"$RESET
-            docker restart cassandra
-
-            if [ -n "$(docker ps | grep cassandra)" ]
-            then
-              echo "Waiting for port to open"
-              while [ -n "$(docker exec -it cassandra cqlsh -e exit 2>&1 | grep '\(e\|E\)rror')" ]
-              do
-                sleep 1
-              done
-              echo "Cqlsh is up and running"
-            fi
-
+        if [ -n "$(docker ps | grep cassandra)" ]
+        then
+          echo "Waiting for port to open"
+          while [ -n "$(docker exec -it cassandra cqlsh -e exit 2>&1 | grep '\(e\|E\)rror')" ]
+          do
+            sleep 1
+          done
+          echo "Cqlsh is up and running"
+          echo "Running schema"
+          docker exec -it cassandra cqlsh -f /schema.cql
         fi
 
+    else
+        echo -e $GREEN"### Restarting cassandra container"$RESET
+        docker restart cassandra
 
+        if [ -n "$(docker ps | grep cassandra)" ]
+        then
+          echo "Waiting for port to open"
+          while [ -n "$(docker exec -it cassandra cqlsh -e exit 2>&1 | grep '\(e\|E\)rror')" ]
+          do
+            sleep 1
+          done
+          echo "Cqlsh is up and running"
+        fi
+
+    fi
+}
+
+function graphite {
         # Graphite container
         # Ports: Receive=2003, WebApp=2000
         if [ ! "$(docker ps --all | grep graphite)" ]; then
@@ -95,87 +93,125 @@ in
             echo -e $GREEN"### Restarting graphite container"$RESET
             docker restart graphite
         fi
+}
 
-        # Backend container
-        # Port: 8080
-        # echo -e $YELLOW"### Cleaning backend container"$RESET
-        # if [[ "$(docker ps | grep backend)" ]]; then
-        #     docker stop backend
-        # fi
-        # if [[ "$(docker ps --all | grep backend)" ]]; then
-        #     docker rm backend
-        # fi
-        # if [[ "$(docker images -q $BACKEND 2> /dev/null)" == "" ]]; then
-        #     docker rmi $BACKEND
-        # fi
-        # echo -e $YELLOW"### Compiling backend container"$RESET
-        # (cd ./QvantelBackend; sbt assembly)
-        # echo -e $YELLOW"### Building backend container"$RESET
-        # docker build -t backend ./QvantelBackend
-        # echo -e $GREEN"### Starting backend container"$RESET
-        # docker run \
-            # --restart=always \
-            # --name backend \
-            # -p 8080:8080 \
-            # -d $BACKEND
+function backend {
+    # Backend container
+    # Port: 8080
+    # echo -e $YELLOW"### Cleaning backend container"$RESET
+    # if [[ "$(docker ps | grep backend)" ]]; then
+    #     docker stop backend
+    # fi
+    # if [[ "$(docker ps --all | grep backend)" ]]; then
+    #     docker rm backend
+    # fi
+    # if [[ "$(docker images -q $BACKEND 2> /dev/null)" == "" ]]; then
+    #     docker rmi $BACKEND
+    # fi
+    # echo -e $YELLOW"### Compiling backend container"$RESET
+    # (cd ./QvantelBackend; sbt assembly)
+    # echo -e $YELLOW"### Building backend container"$RESET
+    # docker build -t backend ./QvantelBackend
+    # echo -e $GREEN"### Starting backend container"$RESET
+    # docker run \
+        # --restart=always \
+        # --name backend \
+        # -p 8080:8080 \
+        # -d $BACKEND
+   ''
+}
 
-	# CDRGenerator container
-        echo -e $YELLOW"### Cleaning CDRGenerator container"$RESET
-        if [[ "$(docker ps | grep cdrgenerator)" ]]; then
-            docker stop cdrgenerator
-        fi
-        if [[ "$(docker ps --all | grep cdrgenerator)" ]]; then
-            docker rm cdrgenerator
-        fi
-        if [[ "$(docker images -q $CDRGENERATOR 2> /dev/null)" == "" ]]; then
-            docker rmi $CDRGENERATOR
-        fi
-        echo -e $YELLOW"### Compiling CDRGenerator container"$RESET
-        (cd ./QvantelCDRGenerator; sbt assembly)
-        echo -e $YELLOW"### Building CDRGenerator container"$RESET
-        docker build -t cdrgenerator ./QvantelCDRGenerator
-        echo -e $GREEN"### Starting CDRGenerator container"$RESET
-        docker run \
-            --restart=always \
-	    --net=host \
-            --name cdrgenerator \
-            -d $CDRGENERATOR
+function cdrgenerator {
+    # CDRGenerator container
+    echo -e $YELLOW"### Cleaning CDRGenerator container"$RESET
+    if [[ "$(docker ps | grep cdrgenerator)" ]]; then
+        docker stop cdrgenerator
+    fi
+    if [[ "$(docker ps --all | grep cdrgenerator)" ]]; then
+        docker rm cdrgenerator
+    fi
+    if [[ "$(docker images -q $CDRGENERATOR 2> /dev/null)" == "" ]]; then
+        docker rmi $CDRGENERATOR
+    fi
+    echo -e $YELLOW"### Compiling CDRGenerator container"$RESET
+    (cd ./QvantelCDRGenerator; sbt assembly)
+    echo -e $YELLOW"### Building CDRGenerator container"$RESET
+    docker build -t cdrgenerator ./QvantelCDRGenerator
+    echo -e $GREEN"### Starting CDRGenerator container"$RESET
+    docker run \
+        --restart=always \
+        --net=host \
+        --name cdrgenerator \
+        -d $CDRGENERATOR
+}
 
+function dbconnector {
 	# DBConnector container
-        echo -e $YELLOW"### Cleaning DBConnector container"$RESET
-        if [[ "$(docker ps | grep dbconnector)" ]]; then
-            docker stop dbconnector
-        fi
-        if [[ "$(docker ps --all | grep dbconnector)" ]]; then
-            docker rm dbconnector
-        fi
-        if [[ "$(docker images -q $DBCONNECTOR 2> /dev/null)" == "" ]]; then
-            docker rmi $DBCONNECTOR
-        fi
-        echo -e $YELLOW"### Compiling DBConnector container"$RESET
-        (cd ./QvantelDBConnector; sbt assembly)
-        echo -e $YELLOW"### Building DBConnector container"$RESET
-        docker build -t dbconnector ./QvantelDBConnector
-        echo -e $GREEN"### Starting DBConnector container"$RESET
-        docker run \
-            --restart=always \
-	    --net=host \
-            --name dbconnector \
-            -d $DBCONNECTOR
+    echo -e $YELLOW"### Cleaning DBConnector container"$RESET
+    if [[ "$(docker ps | grep dbconnector)" ]]; then
+        docker stop dbconnector
+    fi
+    if [[ "$(docker ps --all | grep dbconnector)" ]]; then
+        docker rm dbconnector
+    fi
+    if [[ "$(docker images -q $DBCONNECTOR 2> /dev/null)" == "" ]]; then
+        docker rmi $DBCONNECTOR
+    fi
+    echo -e $YELLOW"### Compiling DBConnector container"$RESET
+    (cd ./QvantelDBConnector; sbt assembly)
+    echo -e $YELLOW"### Building DBConnector container"$RESET
+    docker build -t dbconnector ./QvantelDBConnector
+    echo -e $GREEN"### Starting DBConnector container"$RESET
+    docker run \
+        --restart=always \
+        --net=host \
+        --name dbconnector \
+        -d $DBCONNECTOR
+}
 
-        # Frontend container
-        # Port: 3000
-        if [ ! "$(docker ps --all | grep frontend)" ]; then
-            echo -e $YELLOW"### Creating frontend container"$RESET
-            docker run --name frontend \
-                --restart=always \
-                -p 3000:3000 \
-                -v $GRAFANA_VOLUME_TARGET:/var/lib/grafana \
-                -d $GRAFANA
-        else
-            echo -e $GREEN"### Restarting frontend container"$RESET
-            docker restart frontend
-        fi
+function frontend {
+    # Frontend container
+    # Port: 3000
+    if [ ! "$(docker ps --all | grep frontend)" ]; then
+        echo -e $YELLOW"### Creating frontend container"$RESET
+        docker run --name frontend \
+            --restart=always \
+            -p 3000:3000 \
+            -v $GRAFANA_VOLUME_TARGET:/var/lib/grafana \
+            -d $GRAFANA
+    else
+        echo -e $GREEN"### Restarting frontend container"$RESET
+        docker restart frontend
+    fi
+}
+case "$1"
+in
+    "start")
+        case "$2"
+        in
+            "cass"|"cassandra")
+                cassandra
+            ;;
+            "cdr"|"cdrgenerator")
+                cdrgenerator
+            ;;
+            "graphite")
+                graphite
+            ;;
+            "dbc"|"dbconncetor")
+                dbconnector
+            ;;
+            "frontend")
+                frontend
+            ;;
+            *)
+                graphite
+                cassandra
+                cdrgenerator
+                dbconnector
+                frontend
+            ;;
+         esac
     ;;
     "stop")
         echo -e $RED"Stopping containers"$RESET
