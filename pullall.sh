bb9282ba46c8871284207024a85d9219be6666f1
Johan Bjäreholt <johan@bjareho.lt>
2017-03-22 09:47:24 +0100

    Git repo and pullall now tracks master on all submodules

--- pullall.sh
+++ pullall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git pull
 git submodule update --init --recursive --remote
+git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
