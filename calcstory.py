4eb577cfad3a4137b1ac5aaacb2ed369046820e8
Oliver Örnmyr <Lillfilly@hotmail.com>
2017-03-27 11:41:29 +0200

    calcstory.py is now listing amount of unestimated tasks. It is now easier to change which asana project calcstory should use.

--- calcstory.py
+++ calcstory.py
@@ -3,12 +3,16 @@ import json
 import re
 
 #HTTP Request data
-url = 'https://app.asana.com/api/1.0/projects/297767546500356/tasks?opt_expand=this'
+#projectId = '278035678015737' #Sprint 1
+projectId = '297767546500356' #Sprint 2
+
+url = 'https://app.asana.com/api/1.0/projects/'+projectId+'/tasks?opt_expand=this'
 headr = {'Authorization': 'Bearer 0/5365430af91365ad8b0df5a4d0a07078'}
 
 #Counters
 completedPoints = 0
 totalPoints = 0
+unestimatedTasks = 0
 
 #Regex to find a number within parentheses. Dots also allowed e.g (3.4)
 regex = r".*?\(([0-9\.]+)\).*?" 
@@ -23,13 +27,16 @@ taskList = json.loads(response.text)['data']
 
 #Loop the tasklist and count the total and completed points
 for task in taskList:
-  regmatch = re.search(regex, task['name'])#Search for regex match within the task name
-  if(regmatch):
-    taskPoints = float(regmatch.group(1))#Get the amount of points for the task
-    totalPoints += taskPoints #Add it to the total
-    if(task['completed']):
-      completedPoints += taskPoints  #if task is completed, add points to complete count
+    regmatch = re.search(regex, task['name'])#Search for regex match within the task name
+    if(regmatch):
+        taskPoints = float(regmatch.group(1))#Parse the story points for the task
+        totalPoints += taskPoints
+        if(task['completed']):
+            completedPoints += taskPoints  #if task is completed, add points to complete count
+    else:
+        unestimatedTasks += 1 #If the task did not have an estimation
 
 #Print the result
 print('Total: ', totalPoints)
 print('Completed: ', completedPoints)
+print('Amount of unestimated tasks: ', unestimatedTasks)
