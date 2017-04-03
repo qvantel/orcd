cd712786697d7b40ac26ad83aeec8e3c5c051fe0
Erik Lilja <erikvlilja@gmail.com>
2017-04-03 16:14:47 +0200

    Rename application conf. path

--- test/cdr-cass.sh
+++ test/cdr-cass.sh
@@ -6,7 +6,7 @@ cass_container_name=$1
 
 # Paths
 repo="QvantelCDRGenerator"
-app_conf_path="application.conf"
+app_conf_path="src/main/resource/application.conf"
 
 # What in config file needs to be updated(for intgr. test)
 batch_limit="batch\.limit"
@@ -23,10 +23,11 @@ temp_cassandra_result_file="out"
 timeout=45
 
 # Checks, exit if param empty
-# [ -n "$1" ] || { echo "empty params[no success]"; exit 1; }
+[ -n "$1" ] || { echo "empty params[no success]"; exit 1; }
 
 # Same, except if not a docker container.
-# [ -z "$(docker ps | grep $1)" ] && { echo "The string the argument '$1' provided is not a running container. [no success]"; exit 1; }
+[ -z "$(docker ps | grep $1)" ] && { echo "The string the argument '$1' provided is not a running container. [no success]"; exit 1; }
+
 
 # Use the stack to navigate to the submodule
 pushd "../$repo" 2>&1 1>/dev/null
@@ -51,6 +52,9 @@ sed -i "s#${cassandra_port}\ *\=\ *\"[0-9]*\"#${cassandra_port}\=\"${cassandra_i
 grep -q "${batch_limit}=1" "$app_conf_path" 2>&1 && echo -n "" || { echo "Failed to set batch limit [no success]"; exit 1; }
 grep -q "${batch_size_limit}=1" "$app_conf_path" && echo -n "" ||  { echo "Failed to set batch size limit [no success]"; exit 1; }
 grep -q "${cassandra_port}=\"${cassandra_it_port}\"" "$app_conf_path" && echo -n "" || { echo "Failed to set cassandra port [no success]"; exit 1; }
+
+# Compile CDRGenerator.jar if it doesn't exist.
+if [ ! -f "$jar_directory" ] && sbt assembly
 
 # Finally, run the jar.
 # Run with timeout
