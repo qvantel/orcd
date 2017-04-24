f671eefc9107aeff67652377566a750646c2807a
filip brannlud stal <filip.brannlundstal@gmail.com>
2017-04-24 13:32:40 +0200

    Added Disk space total

--- resourceUsageCassandra.sh
+++ resourceUsageCassandra.sh
@@ -12,6 +12,10 @@ while true; do
   USED_MEM=$(free -m | awk '{print $3}' | sed -n 2p)
   # converting used memory in MB into gigabytes
   USED_MEM_GB=$(bc <<< "scale=2 ; $USED_MEM / $GB_DIVIDER")
+  # total memory in MB
+  TOTAL_MEM=$(free -m | awk '{print $2}' | sed -n 2p)
+  # converting total memory in MB into gigabytes
+  TOTAL_MEM_GB=$(bc <<< "scale=2 ; $TOTAL_MEM / $GB_DIVIDER")
 
 
   # Free memory in MB
@@ -20,16 +24,23 @@ while true; do
   FREE_MEM_GB=$(bc <<< "scale=2 ; $FREE_MEM / $GB_DIVIDER")
 
 
-  #  Available space in disk in MB
-  AVAILABLE_SPACE=$(df -m --total | tail -1 | awk '{print $4}')
-  # converting available space in MB into gigabytes
-  AVAILABLE_SPACE_GB=$(bc <<< "scale=2 ; $AVAILABLE_SPACE / $GB_DIVIDER")
+  #  Used space in disk in MB
+  USED_SPACE=$(df -m --total | tail -1 | awk '{print $3}')
+  # converting used space in MB into gigabytes
+  USED_SPACE_GB=$(bc <<< "scale=2 ; $USED_SPACE / $GB_DIVIDER")
+  #  Total space in disk in MB
+  TOTAL_SPACE=$(df -m --total | tail -1 | awk '{print $2}')
+  # total available space in MB into gigabytes
+  TOTAL_SPACE_GB=$(bc <<< "scale=2 ; $TOTAL_SPACE / $GB_DIVIDER")
 
+  echo "cassandra.cpu.usage ${CPU} `date +%s`" | nc ${SERVER} ${PORT}
 
   echo "cassandra.memory.used $USED_MEM_GB `date +%s`" |  nc ${SERVER} ${PORT}
-  echo "cassandra.cpu.usage ${CPU} `date +%s`" | nc ${SERVER} ${PORT}
   echo "cassandra.memory.free ${FREE_MEM_GB} `date +%s`" | nc ${SERVER} ${PORT}
-  echo "cassandra.disk.available ${AVAILABLE_SPACE_GB} `date +%s`" | nc ${SERVER} ${PORT}
+  echo "cassandra.memory.total ${TOTAL_MEM_GB} `date +%s`" | nc ${SERVER} ${PORT}
 
-  sleep 2;
+  echo "cassandra.disk.used ${USED_SPACE_GB} `date +%s`" | nc ${SERVER} ${PORT}
+  echo "cassandra.disk.total ${TOTAL_SPACE_GB} `date +%s`" | nc ${SERVER} ${PORT}
+
+  #sleep 2;
 done
