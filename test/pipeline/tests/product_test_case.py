0b12f7e0f7b59815b549ddd6c674f5739a768c3e
Erik Lilja <erikvlilja@gmail.com>
2017-05-03 11:55:54 +0200

    Change metric path in test case

--- test/pipeline/tests/product_test_case.py
+++ test/pipeline/tests/product_test_case.py
@@ -8,23 +8,40 @@ from utils import *
 class ProductTestCase(unittest.TestCase):
     """Test cases for Products."""
 
+
     def setUp(self):
         self.client = Client()
 
-    def test_product_data_plan_extra(self):
-        """Testing response of qvantel.product.Dataplanextra."""
-        response = self.client.request(format_url_with_target("qvantel.product.Dataplanextra"))
+
+    def test_product_data_freefacebook(self):
+        """Testing response of qvantel.product.data.FreeFacebook"""
+        response = self.client.request(format_url_with_target(metric_path("freefacebook")))
         self.assert_datapoints(response)
 
-    def test_product_data_plan_normal(self):
-        """Testing response of qvantel.product.Dataplannormal."""
-        response = self.client.request(format_url_with_target("qvantel.product.Dataplannormal"))
+
+    def test_product_voice_callsafter22(self):
+        """Testing response of qvantel.product.voice.callsafter22"""
+        response = self.client.request(format_url_with_target(metric_path("callsafter22")))
         self.assert_datapoints(response)
 
-    def test_product_data_plan_world(self):
-        """Testing response of qvantel.product.Dataplanworld."""
-        response = self.client.request(format_url_with_target("qvantel.product.Dataplanworld"))
+
+    def test_product_sms_smsplannormal(self):
+        """Testing response of qvantel.product.sms.SMSPlanNormal"""
+        response = self.client.request(format_url_with_target(metric_path("smsplannormal")))
         self.assert_datapoints(response)
+
+
+    def test_product_mms_mmsplannormal(self):
+        """Testing response of qvantel.product.mms.MMSPlanNormal"""
+        response = self.client.request(format_url_with_target(metric_path("mmsplannormal")))
+        self.assert_datapoints(response)
+
+
+    def test_product_voice_callplannormal(self):
+        """Testing response of qvantel.product.voice.callplannormal"""
+        response = self.client.request(format_url_with_target(metric_path("callplannormal")))
+        self.assert_datapoints(response)
+
 
     def assert_datapoints(self, data):
         for element in data:
--- test/pipeline/utils.py
+++ test/pipeline/utils.py
@@ -1,5 +1,18 @@
 from config import *
 
+maps = {
+    "callplannormal": "qvantel.products.voice.CallPlanNormal",
+    "callsafter22": "qvantel.products.voice.CallsAfter22",
+    "smsplannormal": "qvantel.products.sms.SMSPlanNormal",
+    "mmsplannormal": "qvantel.products.mms.MMSPlanNormal",
+    "freefacebook": "qvantel.products.data.FreeFacebook"
+}
+
 def format_url_with_target(target):
     url = "http://" + graphite_host + ":" + graphite_port + "/render?target=" + target + "&format=json&from=-50s&until"
     return url
+
+
+# Returns the metric patch that Graphite uses.
+def metric_path(metric):
+    return maps[metric]
