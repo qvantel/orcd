fdbfc75a18b0f9c98d0b0b70c0ea86ff82de7647
Max Meldrum <max@meldrum.se>
2017-05-08 14:50:46 +0200

    Remove newline

--- create_demo_kit.sh
+++ create_demo_kit.sh
@@ -3,7 +3,6 @@
 GRAPHITE_CONTAINER="graphite"
 GRAPHITE_DEMO_PORT=2000
 
-
 # Extract data from whisper into a tar file
 # Note: backup.tar is around 2.7G
 docker run --rm --volumes-from $GRAPHITE_CONTAINER -v $(pwd):/backup ubuntu tar cvf /backup/backup.tar /opt/graphite/storage/whisper
