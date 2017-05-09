12f6653752f2766e64e87429aae4e429aa06d10b
Erik Lilja <erikvlilja@gmail.com>
2017-05-09 16:44:10 +0200

    Add dependency check

---
+++ before_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+test -f "$(which docker )" || { echo "Docker not installed"; exit 1; }
+test -f "$(which npm)" || { echo "NPM not installed"; exit 1; }
+test -f "$(which node)" || { echo "Node not installed";
+    test -f "$(which nodejs)" && echo "NodeJs is installed however!";
+    exit 1; }
+test -f "$(which md5sum )" || { echo "md5sum not installed"; exit 1; }
+test -f "$(which wget )" || { echo "wget not installed"; exit 1; }
+test -f "$(which java )" || { echo "java not installed"; exit 1; }
+test -f "$(which sbt )" || { echo "sbt not installed"; exit 1; }
+test -f "$(which scala2 )" || { echo "scala not installed"; exit 1; }
