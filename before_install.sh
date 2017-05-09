8c2ec77e835c877fc8d9d358c22ffb15a9e11790
Erik lilja <erik.liljaa@live.se>
2017-05-09 19:35:11 +0200

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -3,7 +3,8 @@
 test -f "$(which docker )" || { echo "Docker not installed"; exit 1; }
 test -f "$(which npm)" || { echo "NPM not installed"; exit 1; }
 test -f "$(which node)" || { echo "Node not installed";
-    test -f "$(which nodejs)" && echo "NodeJs is installed however!";
+    node_path=$(which nodejs)
+    test -f "$(which nodejs)" && echo "Please symlink $(which nodejs) to $(dirname $k)/node";
     exit 1; }
 test -f "$(which md5sum )" || { echo "md5sum not installed"; exit 1; }
 test -f "$(which wget )" || { echo "wget not installed"; exit 1; }
