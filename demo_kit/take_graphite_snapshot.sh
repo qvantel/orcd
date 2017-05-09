66c20e88d3710425c940f8150b86d5372d04c1c4
Max Meldrum <max@meldrum.se>
2017-05-09 14:51:32 +0200

    Fix name typo

--- demo_kit/take_graphite_snapshot.sh
+++ demo_kit/take_graphite_snapshot.sh
@@ -4,6 +4,6 @@ GRAPHITE_CONTAINER="graphite"
 GRAPHITE_DEMO_PORT=2000
 
 # Extract data from whisper into a tar file
-# Note: backup.tar is around 2.7G
+# Note: demo_kit.tar is around 2.7G
 docker run --rm --volumes-from $GRAPHITE_CONTAINER -v $(pwd):/backup ubuntu tar cvf /backup/demo_kit.tar /opt/graphite/storage/whisper
 
